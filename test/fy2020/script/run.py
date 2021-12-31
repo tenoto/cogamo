b41bef7d188527a3de313503aae655a829a530d1
tenoto <teruaki.enoto@gmail.com>
2021-12-31 10:36:38 +0900

    modify

--- test/fy2020/script/run.py
+++ test/fy2020/script/run.py
@@ -3,3 +3,26 @@
 import cogamo.cogamo as cogamo
 
 cogamo.run('script/parameter.yaml')
+
+exit()
+
+pipe = cogamo.Pipeline('script/parameter.yaml')
+pipe.write()
+
+cat = cogamo.Catalog('script/parameter.yaml')
+cat.write()
+
+pipe.set_catalog(cat)
+pipe.process(3)
+pipe.write()
+
+exit()
+
+for index in range(len(pipe.df)):
+	try:
+		pipe.process(index)
+		pipe.write()
+	except:
+		continue
+
+
