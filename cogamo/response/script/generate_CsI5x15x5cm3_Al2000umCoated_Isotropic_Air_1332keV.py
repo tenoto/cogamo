9e2935ed780fbe22458679adf874bea1ed69d7bb
Teruaki Enoto <teruaki.enoto@riken.jp>
2022-05-28 10:36:58 +0900

    script

--- cogamo/response/script/generate_CsI5x15x5cm3_Al2000umCoated_Isotropic_Air_1332keV.py
+++ cogamo/response/script/generate_CsI5x15x5cm3_Al2000umCoated_Isotropic_Air_1332keV.py
@@ -1,4 +1,4 @@
 #!/bin/sh -f 
 
-../../cogamo/response/generate_cogamo_response.py \
-../../cogamo/response/parameter/cogamo_fy2020_CsI5x15x5cm3_Al2000umCoated_Isotropic_Air_1332keV_rsp.yaml
+generator/generate_cogamo_response.py \
+parameter/cogamo_fy2020_CsI5x15x5cm3_Al2000umCoated_Isotropic_Air_1332keV_rsp.yaml
